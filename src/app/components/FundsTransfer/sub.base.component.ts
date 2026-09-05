c4e6b3bb687f71b54866d2167161ff719f126cf0
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-05 15:38:08 -0400

    Initial commit

--- src/app/components/FundsTransfer/sub.base.component.ts
+++ src/app/components/FundsTransfer/sub.base.component.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { BaseComponent } from '../base.component';
 
-/** 
+import { Directive } from '@angular/core';
+
+/**
 	Base class of all FundsTransfer Edit and Create Components.  
-**/
+ **/
+@Directive()
 export class SubBaseComponent extends BaseComponent {
 
   constructor (http: HttpClient) { super(http); }
